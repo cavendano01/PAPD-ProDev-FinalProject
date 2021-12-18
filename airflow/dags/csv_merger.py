8891a4974ed79ff224c52bd198f8929411ca6f71
carlos Avendaño <carlos@unplug.studio>
2021-12-18 05:43:40 -0600

    Project Up and Running

--- airflow/dags/csv_merger.py
+++ airflow/dags/csv_merger.py
@@ -20,23 +20,23 @@ DF_COLUMNS = {
     "Long" : "lon"
 }
 
-FILE_CONNECTION_NAME = 'fs_default'
-CONNECTION_DB_NAME = 'mysql_default'
+FILE_CONNECTION_NAME = 'monitor_file'
+CONNECTION_DB_NAME = 'mysql_db'
 
 def etl_process(**kwargs):
     mysql_connection = MySqlHook(mysql_conn_id=CONNECTION_DB_NAME).get_sqlalchemy_engine()
     logger.info(kwargs["execution_date"])
     file_path = FSHook(FILE_CONNECTION_NAME).get_path()
 
-    for csv_file in glob(file_path+"data/time_series_covid19_*_global.csv"):
+    for covid_file in glob(file_path+"data/time_series_covid19_*_global.csv"):
 
-        match = re.search('time_series_covid19_(confirmed|deaths|recovered)_global.csv', os.path.basename(csv_file))
+        match = re.search('time_series_covid19_(confirmed|deaths|recovered)_global.csv', os.path.basename(covid_file))
         if match:
-            logger.info("Processing csv file " + csv_file)
+            logger.info("Processing Covid-19 file " + covid_file)
             covid_status = match.group(1)
 
             #Read csv file as pandas DataFrame
-            df = (pd.read_csv(csv_file)
+            df = (pd.read_csv(covid_file)
                   .rename(columns=DF_COLUMNS))
             df.state = df.state.fillna('n/a')
             column_names = df.columns.values.tolist()
@@ -48,7 +48,7 @@ def etl_process(**kwargs):
             transformed_df = pd.concat(df_data, axis=1)
 
 
-            #transform dates columns to rows
+            #Use pandas melt function to transform dates columns to rows
             transformed_df = transformed_df.melt(id_vars=column_names[0:4],
                                      value_vars=column_names[4:],
                                      var_name='date', value_name='cases')
@@ -60,14 +60,14 @@ def etl_process(**kwargs):
             #Insert transformed DataFrame into
 
             with mysql_connection.begin() as connection:
-                connection.execute("DELETE FROM covid19.merged WHERE status='"+covid_status+"'")
-                transformed_df.to_sql('global_data', con=connection, schema='padp_db', if_exists='append', index=False)
+                connection.execute("DELETE FROM covid19.data WHERE status='"+covid_status+"'")
+                transformed_df.to_sql('data', con=connection, schema='covid19', if_exists='append', index=False)
 
 
 
             logger.info(f"Rows inserted {len(df.index)}")
         else:
-            logger.warning("Not valid covid filename status: " + csv_file)
+            logger.warning("Not valid covid filename status: " + covid_file)
 
     #Clean all files
     logger.info("Cleaning files...")
@@ -86,14 +86,14 @@ dag = DAG('csv_merger', description='A simple merge and transform of CSVs on dat
           schedule_interval='0 1 * * *',
           catchup=False)
 
-sensor = FileSensor(task_id="file_sensor",
+sensor = FileSensor(task_id="file_sensor_task",
                     dag=dag,
                     filepath="data/",
                     fs_conn_id=FILE_CONNECTION_NAME,
                     poke_interval=10,
                     timeout=30)
 
-etl = PythonOperator(task_id="csv_merger_etl",
+etl = PythonOperator(task_id="covid19_etl",
                      provide_context=True,
                      python_callable=etl_process,
                      dag=dag
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,16 +1,47 @@
-# import pathlib
-# import pydicom
 import streamlit as st
-import mysql.connector as connection
+import numpy as np
 import pandas as pd
-
-# SETTING PAGE CONFIG TO WIDE MODE
-st.set_page_config(layout="wide")
+import mysql.connector as connection
 
 
-mydb = connection.connect(host="mysql_db", database='padp_db', user="padp_user", passwd="padp_pw", use_pure=True)
-query = "Select * from covid19.merged;"
-df = pd.read_sql(query, mydb)
-mydb.close()  # close the connection
+st.set_page_config(
+     page_title="COVID19 Dashboard",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get help': 'https://cavendano01.notion.site/My-Dashboard-Help-Center-06578557c1674760ab4b84c068da4b6f',
+         'About': "https://github.com/cavendano01/PAPD-ProDev-FinalProject"
+     }
+ )
 
-st.table(df)
+st.title('COVID19 Dashboard')
+
+# Creating Data Frame
+mydb = connection.connect(host="mysql_db", database = 'covid19',user="covid19", passwd="secretpass",use_pure=True)
+query = "Select * from covid19.data;"
+df = pd.read_sql(query,mydb)
+mydb.close() #close the connection
+
+st.table(df.head(100))
+
+def filter_df(df, start_date, end_date, countries, states):
+    mask = (df['date'] >= start_date) & (df['date'] <= end_date)
+    if len(countries) > 0:
+        mask &= (df['country'].isin(countries))
+
+    if len(states) > 0:
+        mask &= (df['state'].isin(states))
+
+    return df[mask]
+
+def get_cases_count(df, grouby, status=["confirmed", "recovered","deaths"]):
+    # delta_df = df[(df['status'] == status)].groupby(['country', 'state', 'lat', 'lon'], dropna=True)['count'].agg(['first', 'last']).reset_index()
+    # delta_df['count'] = delta_df['last'] - delta_df['first']
+    # delta_df["count_str"] = delta_df["count"].map('{:,d}'.format)
+    # delta_df.drop(columns=['first', 'last'], inplace=True)
+    count_df = df[(df['status'].isin(status))].groupby(grouby).agg({'cases_int': 'sum'}).reset_index()
+    count_df["cases"] = count_df["cases_int"].map('{:,d}'.format)
+    return count_df[(count_df["cases_int"] >= 0)]
+
+covid_df = df
