ee4f0802495110981ad3de56d06c6ff5fdf0f8f0
carlos Avendaño <carlos@unplug.studio>
2021-12-18 01:30:18 -0600

    Updated Baseline for Project

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,10 +1,21 @@
-import pathlib
-import pydicom
+# import pathlib
+# import pydicom
 import streamlit as st
+import mysql.connector as connection
+import pandas as pd
 
 
-st.title("Hello World!")
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
 
-st.write(pathlib.Path.home())
+try:
+    mydb = connection.connect(host="mysql_db", database = 'covid19',user="covid19", passwd="secretpass",use_pure=True)
+    query = "Select * from covid19.global_data;"
+    result_dataFrame = pd.read_sql(query,mydb)
+    mydb.close() #close the connection
+except Exception as e:
+    mydb.close()
+    print(str(e))
 
-st.write(pydicom)
+
+st.table(result_dataFrame)
